8e0e060305bd6f4eef0c8c1b371d8e411357731a
Donia Tung <doniatung1@Donias-MacBook-Pro.local>
2018-11-03 14:31:48 -0400

    hi

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main{
 
 
+  public static void main(String[] args){
+    HashMap<String, HashMap<String, Integer>> observations = Training.fileToObv("text/simple-test-sentences", "simple-test-tags");
+    HashMap<String, HashMap<String, Integer>> transitions = Training.fileToTrans("text/simple-test-tags");
+    System.out.println(observations);
+    System.out.println(transitions);
+  }
+
 
 }
